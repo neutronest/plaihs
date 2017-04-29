b729f956cfdf389b91e759396704f1f4276731ed
neutronest <neutronest@gmail.com>
2017-04-29 18:24:31 +0900

    ms

--- src/Main.hs
+++ src/Main.hs
@@ -26,7 +26,11 @@ data ExprC a where
   PlusC :: ExprC Int -> ExprC Int -> ExprC Int
   MultC :: ExprC Int -> ExprC Int -> ExprC Int
   VarC :: Symbol -> ExprC a
+  -- func :: name -> param -> body
+  FunC :: Symbol -> Symbol -> ExprC a
+  -- apply :: arg (as expr)-> body
   AppC :: ExprC a -> ExprC a
+  -- lambda expression :: arg (as expr) -> body
   LamC :: Symbol -> ExprC a
   SetC :: Symbol -> ExprC a
   SeqC :: ExprC a -> ExprC a
@@ -53,6 +57,8 @@ data Env a = Env [Binding a]
 data Cell a = Cell (Location, Value a)
 data Store a = Store [Cell a]
 
+data Result v s = Result (Value v, Store s)
+
 lookupLoc :: Symbol -> Env -> Maybe Location
 lookupLoc sym [] = Nothing
 lookupLoc sym ((sym_, loc_): binds) =
@@ -65,6 +71,13 @@ lookupVal loc (Store ( (Cell(loc_, val_)):cells )) =
   if loc == loc_ then (Just val_)
   else lookupVal loc (Store cells)
 
+
+
+interp :: ExprC a -> Env -> Store a -> Maybe(Result a a)
+interp expr env store =
+  case expr of
+    NumC num -> Just $ Result ((NumV num), store)
+    _ -> Nothing
 
 -- interp :: [ExprC a] -> 
 
